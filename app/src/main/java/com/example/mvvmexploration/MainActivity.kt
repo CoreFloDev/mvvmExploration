bf83f9e8b9e3599d6a6ce634dcd7943ca1bfe90b
Pawel <p.szymczak.0@gmail.com>
2021-10-11 18:40:01 +0100

    Tidy up

--- app/src/main/java/com/example/mvvmexploration/MainActivity.kt
+++ app/src/main/java/com/example/mvvmexploration/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.mvvmexploration
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mvvmexploration.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,7 +23,9 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main)!!
+                .findNavController()
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
--- app/src/main/java/com/example/mvvmexploration/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/mvvmexploration/dashboard/DashboardFragment.kt
@@ -1,44 +1,23 @@
 package com.example.mvvmexploration.dashboard
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.example.mvvmexploration.databinding.FragmentDashboardBinding
+import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 
 class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private val dashboardViewModel by viewModels<DashboardViewModel>()
+    private val binding by viewBinding(FragmentDashboardBinding::bind)
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        dashboardViewModel.text.observe(viewLifecycleOwner, {
             textView.text = it
         })
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        super.onViewCreated(view, savedInstanceState)
     }
 }
